99cf46b71272c0529bd4d9f8f90597c8779454d3
phimrisa <phimrisa@gmail.com>
2026-02-22 16:48:46 +0700

    Update BankAccount.java

--- Banking/BankAccount.java
+++ Banking/BankAccount.java
@@ -1,7 +1,6 @@
 package Banking;
 
 public class BankAccount {
-
     private String accId;
     private double accBalance;
     private final String filename = "src/Banking/users_account.txt";
@@ -10,24 +9,31 @@ public class BankAccount {
         this.accId = accId;
         this.accBalance = accBalance;
     }
+    
     public BankAccount(String accId) {
         this(accId,0.0);
     }
+    
     public BankAccount() {
         this("",0.0);
     }
+    
     public String getAccId() {
         return accId;
     }
+    
     public void setAccBalance(double accBalance) {
         this.accBalance = accBalance;
     }
+    
     public double getAccBalance() {
         return accBalance;
     }
+    
     public String getFilename() {
         return filename;
     }
+    
     public String toString() {
         return accId + ", " + accBalance;
     }
